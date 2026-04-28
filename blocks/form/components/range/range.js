5a9596fadf71ac094d7d4a1ddc9f1c0a7cb4f01a
Spoorti <spoortip@adobe.com>
2026-04-28 11:03:09 +0530

    jhg

--- blocks/form/components/range/range.js
+++ blocks/form/components/range/range.js
@@ -47,7 +47,6 @@ export default async function decorate(fieldDiv, fieldJson) {
 
   input.addEventListener('input', (e) => {
     updateBubble(e.target, div);
-    initRangeEnhancer(fieldDiv);
   });
   updateBubble(input, div);
   initRangeEnhancer(fieldDiv);
